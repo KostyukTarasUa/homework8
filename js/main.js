413557b70cff1d38ecea51262bf5909b43749d28
Taras Kostyuk <taraskostyuk26@gmail.com>
2026-01-07 19:14:45 +0200

    fixed js error

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const buttons = document.querySelectorAll('.filter-buttons');
+const buttons = document.querySelectorAll('.portfolio-filter-buttons');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('is-active'));
-        button.classList.add('is-active');
+        buttons.forEach(btn => btn.classList.remove('portfolio-filter-buttons-active'));
+        button.classList.add('portfolio-filter-buttons-active');
     });
 });
