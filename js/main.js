9840e61131f2f9247a5fde46d37803def8b241d6
Taras Kostyuk <taraskostyuk26@gmail.com>
2026-01-07 18:58:44 +0200

    restoring page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const buttons = document.querySelectorAll('.portfolio-filter-buttons');
+const buttons = document.querySelectorAll('.filter-buttons');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('portfolio-filter-buttons-active'));
-        button.classList.add('portfolio-filter-buttons-active');
+        buttons.forEach(btn => btn.classList.remove('is-active'));
+        button.classList.add('is-active');
     });
 });
